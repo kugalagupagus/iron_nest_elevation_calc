313262f4496bfa6af28e60cf4d1a67daef3c39f8
Kugalagupagus <wiktor.skawski@gmail.com>
2026-09-11 15:42:43 +0200

    Add powder charge calculation based on distance

--- calculator.py
+++ calculator.py
@@ -1,9 +1,25 @@
-def elevation_calc(distance, powder_charges):
+def elevation_calc(distance):
     max_elevation = 60
+
+    if distance <= 10:
+        powder_charges = 1
+    elif distance <= 20:
+        powder_charges = 2
+    elif distance <= 30:
+            powder_charges = 3
+    elif distance <= 40:
+            powder_charges = 4
+    elif distance <= 50:
+            powder_charges = 5
+    else:
+          powder_charges = 6
+
     elevation = (distance * max_elevation) / (powder_charges * 10)
-    return elevation
+    return elevation, powder_charges
 
 distance = float(input("Enter distance to target:"))
-powder_charges = float(input("Enter number of powder charges:"))
 
-print(elevation_calc(distance, powder_charges))
+elevation, powder_charges = elevation_calc(distance)
+
+print(f"Elevation: {elevation}")
+print(f"Powder Charges: {powder_charges}")
