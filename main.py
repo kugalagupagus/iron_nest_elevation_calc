2675ad4c02a6f33150746cf73ef5ab7c7146abd7
Kugalagupagus <wiktor.skawski@gmail.com>
2026-09-13 13:23:54 +0200

    Show results in GUI labels instead of console, hide/show dynamically to avoid stale data

--- main.py
+++ main.py
@@ -2,19 +2,35 @@ import tkinter as tk
 from calculator import elevation_calc
 
 def show_value():
+    distance_label.pack_forget()
+    elevation_label.pack_forget()
+    powder_charges_label.pack_forget()
+    status_label.pack_forget()
     try:
         distance = float(entry.get())
         elevation, powder_charges = elevation_calc(distance)
-        print(f"Distance to target: {distance}")
-        print(f"Elevation: {elevation}")
-        print(f"Powder Charges: {powder_charges}")
+        distance_label.pack(before=entry)
+        elevation_label.pack(before=entry)
+        powder_charges_label.pack(before=entry)
+        distance_label.configure(text=f"{distance}")
+        elevation_label.configure(text=f"Elevation: {elevation}")
+        powder_charges_label.configure(text=f"Powder Charges: {powder_charges}")
     except ValueError:
-        print("Value provided is not a float number")
+        status_label.pack(before=entry)
+        status_label.configure(text="Value provided is not a float number")
 
 root = tk.Tk()
 
 label = tk.Label(root, text="Distance (km):")
 label.pack()
+
+status_label = tk.Label(root, text="")
+
+distance_label = tk.Label(root, text="")
+
+elevation_label = tk.Label(root, text="")
+
+powder_charges_label = tk.Label(root, text="")
 
 entry = tk.Entry(root)
 entry.pack()
