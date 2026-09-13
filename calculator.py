0d3457d52e7b28fd9e6d887c1646a8b67fef538a
Kugalagupagus <wiktor.skawski@gmail.com>
2026-09-13 19:25:08 +0200

    Fix bug in calculator.py, ignore PyInstaller build artifacts

--- calculator.py
+++ calculator.py
@@ -1,18 +1,18 @@
 def elevation_calc(distance):
     max_elevation = 60
-    if distance <= 10:
+    if distance <= 5:
         powder_charges = 1
-    elif distance <= 20:
+    elif distance <= 10:
         powder_charges = 2
-    elif distance <= 30:
+    elif distance <= 15:
         powder_charges = 3
-    elif distance <= 40:
+    elif distance <= 20:
         powder_charges = 4
-    elif distance <= 50:
+    elif distance <= 25:
         powder_charges = 5
     else:
         powder_charges = 6
-    elevation = (distance * max_elevation) / (powder_charges * 10)
+    elevation = (distance * max_elevation) / (powder_charges * 5)
     return elevation, powder_charges
 
 if __name__ == "__main__":
